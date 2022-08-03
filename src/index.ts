1d9fd957ffb6a642c60485e15bedaafbe8b9e47b
IanJiang0817 <yang3973@gmail.com>
2022-08-03 16:49:39 +0800

    basic data creation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { DataSource } from 'typeorm';
+import express from 'express';
 import { Banker } from './entities/Banker';
 import { Client } from './entities/Client';
 import { Transaction } from './entities/Transaction';
+import { createClientRouter } from './routes/create_client';
+import { createBankerRouter } from './routes/create_banker';
+
+const app = express();
 
 const myDataSource = new DataSource({
     type: 'postgres',
@@ -18,6 +23,14 @@ const main = async () => {
     try {
         await myDataSource.initialize();
         console.log(`connect to db successfully!`);
+
+        app.use(express.json());
+
+        app.use(createClientRouter);
+        app.use(createBankerRouter);
+        app.listen(8080, () => {
+            console.log('Now listening on port 8080!');
+        });
     } catch (error) {
         console.error(`cannot connect to db`);
         console.log(error);
